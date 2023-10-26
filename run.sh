ae7548bf3382b569f537c9a4cdc6a48f2993931a
UrielArias <urielarias.a12@gmail.com>
2023-10-26 02:09:36 -0300

    adding settings to debug (./run.sh gdb command)

--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-sudo qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev coreaudio,id=audio0 -machine pcspk-audiodev=audio0
+if [[ "$1" = "gdb" ]]; then
+	sudo qemu-system-x86_64 -S -s -hda Image/x64BareBonesImage.qcow2 -m 512 -d int
+else
+	sudo qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev coreaudio,id=audio0 -machine pcspk-audiodev=audio0
+fi
